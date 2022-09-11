0d16106b6de62e1bd7aa9e5bc0a7199e1a9ee8a8
Sébastien Juste <juste.sebastien@proton.me>
2022-09-11 10:58:46 +0200

    modifying of main.py with class_vault.py

---
+++ class_vault.py
@@ -0,0 +1,35 @@
+import getpass
+import zipfile
+import pyminizip
+
+
+
+class Vault:
+    def __init__ (self, login, password):
+        self.login = login
+        self.password = password
+        self.file = f"{login}.csv"
+        self.archive = f"{login}.zip"
+
+    @property
+    def get_login(self):
+        return self.login
+
+    @property
+    def get_password(self):
+        return self.password
+
+    @classmethod
+    def get(cls):
+        login = input("Login: ").lower().strip()
+        password = getpass.getpass()
+        return Vault(login, password)
+
+
+def main():
+    vault = Vault.get()
+    print(vault.file, vault.archive)
+
+
+if __name__ == "__main__":
+    main()
