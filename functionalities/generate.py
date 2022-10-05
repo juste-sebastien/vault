f50bff28c0a7f8254643b30537ebd09568998cfa
Sébastien Juste <juste.sebastien@proton.me>
2022-10-05 19:28:16 +0200

    remaining function to match with interface

--- functionalities/generate.py
+++ functionalities/generate.py
@@ -1,7 +1,8 @@
 import random
+import string
 
 
-def generate():
+def generate(char_numb=0, char_list=[]):
     """
     Generate a random password from the ASCII table, including lower and uppercase,
     numbers, and all specials characters excluded " ' ` and ,
@@ -15,19 +16,50 @@ def generate():
     pwd_created: str
         "pwd_created" is a random password created for the user
     """
+    if char_numb == 0 and char_list == []:
+        char_numb = choose_pwd_length()
+        char_list = choose_special_chars()
+    pwd_created = ""
+    i = 0
+    while i < char_numb:
+        char = random.randint(32, 127)
+        print(char)
+        if not chr(char) in char_list:
+            pass
+        else:
+            pwd_created += chr(char)
+            i += 1
+    return pwd_created
+
+
+def choose_pwd_length():
     try:
         pwd_length = int(input("Which length do you want for your Password? "))
     except (ValueError, TypeError):
         print("You need to type an integer")
-        generate()
+        choose_pwd_length()
     else:
-        pwd_created = ""
-        i = 0
-        while i < pwd_length:
-            char = random.randint(32, 127)
-            if chr(char) in ['"', "'", "`", ","]:
-                pass
-            else:
-                pwd_created += chr(char)
-                i += 1
-        return pwd_created
+        return pwd_length
+
+
+def choose_special_chars():
+    prompt = "What type of characters do you want?\n"+"a for alphabet\n"+"d for digits\n"+"s for specials characters\n"
+    char_list = []
+    special_chars = ["!","\#", "$", "%", "&", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "\\", "."]
+    alphabet = list(string.ascii_lowercase) + list(string.ascii_uppercase)
+    digit = list(string.digits)
+    try:
+        chars_wanted = str(input(prompt))
+    except:
+        print("Accepting only a, d and s")
+        choose_special_chars()
+    else:
+        if "a" in chars_wanted:
+            char_list += alphabet
+        if "d" in chars_wanted:
+            char_list += digit
+        if "s" in chars_wanted:
+            char_list += special_chars
+    return char_list
+
+
