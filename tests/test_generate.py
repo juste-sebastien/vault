78cb31c0736e3beb692c628a8e8d25a96dd37780
Sébastien Juste <juste.sebastien@proton.me>
2022-10-05 21:09:43 +0200

    rewriting test to match with new implementation of generate.py

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -18,3 +18,34 @@ def test_generate():
     assert not '"' in pwd_test == True
     assert not "'" in pwd_test == True
     assert not "`" in pwd_test == True
+
+
+def test_choose_pwd_length(monkeypatch):
+    mock_input = StringIO("15\n")
+    monkeypatch.setattr("sys.stdin", mock_input)
+    assert len(generate.choose_pwd_length()) == 15
+
+
+def test_choose_special_chars(monkeypatch):
+    alphabet = list(string.ascii_lowercase) + list(string.ascii_uppercase)
+    digit = list(string.digits)
+    spec = ["!","\#", "$", "%", "&", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "\\", "."]
+    char_list = alphabet + digit + spec
+    mock_input = StringIO("ads")
+    monkeypatch.setattr("sys.stdin", mock_input)
+    assert generate.choose_special_chars == char_list
+
+    digit = list(string.digits)
+    spec = ["!","\#", "$", "%", "&", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "\\", "."]
+    char_list = digit + spec
+    mock_input = StringIO("ds")
+    monkeypatch.setattr("sys.stdin", mock_input)
+    assert generate.choose_special_chars == char_list
+
+    spec = ["!","\#", "$", "%", "&", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "\\", "."]
+    char_list = spec
+    mock_input = StringIO("s")
+    monkeypatch.setattr("sys.stdin", mock_input)
+    assert generate.choose_special_chars == char_list
+
+
